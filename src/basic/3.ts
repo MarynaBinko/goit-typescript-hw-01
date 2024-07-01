9d36fc2c2f88e169d9a554fd33bdac01e5dce382
Maryna Binko <benyhorses@hotmail.com>
2024-07-01 13:56:53 +0100

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+// Змінна, яка може містити або рядок, або число (union type)
+let variable: string | number;
+
+// Змінна, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type)
+let status: 'enable' | 'disable';
